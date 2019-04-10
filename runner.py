f416264a508eb8630f63993202d487078418ab2a
Giovanni Quattrocchi <giovanniquattrocchi@me.com>
2019-04-10 13:04:16 +0200

    refactoring and fixes

--- runner.py
+++ runner.py
@@ -1,26 +1,27 @@
-from worker import Worker
-from search import Search
+from crawler import Crawler, Crawlers, Search
+
 
 f = open("tokens.txt", "r")
 tokens = f.read().splitlines()
 
-worker = Worker(tokens, lambda crawler, item: crawler.search_code(item))
+crawlers = Crawlers(tokens, Crawler.search_code)
 
-worker.addSearch(Search("terraform", 'resource', {'extension': 'tf'}))
-worker.addSearch(Search("ansible", 'tasks', {'extension': 'yml'}))
-worker.addSearch(Search("puppet_1", 'class', {'extension': 'pp'}))
-worker.addSearch(Search("puppet_2", 'file', {'extension': 'pp'}))
-worker.addSearch(Search("chef", 'Cookbook', {'extension': 'rb'}))
-worker.addSearch(Search("cloudformation", 'AWSTemplateFormatVersion', {'extension': 'yml'}))
-worker.addSearch(Search("compose", 'services', {'extension': 'yml', 'filename': 'docker-compose'}))
-worker.addSearch(Search("kubernetes", 'spec', {'extension': 'yml', 'filename': 'deployments'}))
-worker.addSearch(Search("packer", 'variables+builders', {'extension': 'json'}))
-worker.addSearch(Search("vagrant", 'Vagrant', {'filename' : 'Vagrantfile'}))
-worker.addSearch(Search("cloudify", 'blueprint+nodes', {'extension': 'yml'}))
-worker.addSearch(Search("salt", 'pkg', {'extension': 'sls'}))
-worker.addSearch(Search("brooklyn", 'brooklyn', {'extension': 'yml'}))
-worker.addSearch(Search("tosca_1", 'node_types', {'extension': 'yml'}))
-worker.addSearch(Search("tosca_2", 'node_templates', {'extension': 'yml'}))
+crawlers.addSearch(Search("terraform", 'resource', {'extension': 'tf'}))
+crawlers.addSearch(Search("ansible", 'tasks', {'extension': 'yml'}))
+crawlers.addSearch(Search("puppet_1", 'class', {'extension': 'pp'}))
+crawlers.addSearch(Search("puppet_2", 'file', {'extension': 'pp'}))
+crawlers.addSearch(Search("chef", 'Cookbook', {'extension': 'rb'}))
+crawlers.addSearch(Search("cloudformation", 'AWSTemplateFormatVersion', {'extension': 'yml'}))
+crawlers.addSearch(Search("compose", 'services', {'extension': 'yml', 'filename': 'docker-compose'}))
+crawlers.addSearch(Search("kubernetes", 'spec', {'extension': 'yml', 'filename': 'deployments'}))
+crawlers.addSearch(Search("packer", 'variables+builders', {'extension': 'json'}))
+crawlers.addSearch(Search("vagrant", 'Vagrant', {'filename' : 'Vagrantfile'}))
+crawlers.addSearch(Search("cloudify", 'blueprint+nodes', {'extension': 'yml'}))
+crawlers.addSearch(Search("salt", 'pkg', {'extension': 'sls'}))
+crawlers.addSearch(Search("brooklyn", 'brooklyn', {'extension': 'yml'}))
+crawlers.addSearch(Search("tosca_1", 'node_types', {'extension': 'yml'}))
+crawlers.addSearch(Search("tosca_2", 'node_templates', {'extension': 'yml'}))
 
+crawlers.start()
 
-worker.execute()
+crawlers.join()
--- search.py
+++
@@ -1,5 +0,0 @@
-class Search:
-    def __init__(self, name, text, more):
-        self.name = name
-        self.text = text
-        self.more = more
