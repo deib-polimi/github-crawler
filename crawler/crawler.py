a6216c0bb5e83129ab546e6519f7c590ff682305
Giovanni Quattrocchi <giovanniquattrocchi@me.com>
2019-06-23 20:21:31 +0200

    all repos

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,21 +1,79 @@
 import os
-
+import base64
 from github import Github
 from github.GithubException import GithubException
 from time import sleep
 from random import randint
+from datetime import datetime
 
 DEFAULT_PATH = os.path.join(os.getcwd(), "results")
 
-# default: slot=0.1s, maxWait=1m
+
 class Crawler:
-    def __init__(self, token, slot=0.1, maxWait=60, path=DEFAULT_PATH):
+    def __init__(self, token, slot=0.1, maxWait=25, path=DEFAULT_PATH):
         self.token = token
         self.github = Github(token)
         self.fails = 0
         self.slot = slot
         self.path = path
         self.maxSlots = maxWait//slot
+
+    def getRepos(self, searches):
+        repos = self.github.get_repos()
+        count = 0
+        for r in repos:
+            self.inspectRepo(r, self.genPredicates(searches))
+            count += 1
+            print(count)
+
+    def genPredicates(self, searches):
+        def genPredicateFromSearch(search):
+            def predicate(content, fileName):
+                if search.text:
+                    textSearches = search.text.split("+")
+                    if not all([s in content for s in textSearches]):
+                        return False
+                if 'extension' in search.more and fileName.split('.')[-1] != search.more['extension']:
+                    return False
+                if 'filename' in search.more and fileName != search.more['filename']:
+                    return False
+                return True
+            return predicate
+        res = {}
+        for search in searches:
+            data = res.get(search.name, [])
+            data.append(genPredicateFromSearch(search))
+            res[search.name] = data
+        return res
+
+    def inspectRepo(self, repo, searches):
+        commits = repo.get_commits()
+        numCommits = commits.totalCount
+        if numCommits < 30:
+            return set()
+        lastCommitDate = commits[0].commit.author.date
+        if lastCommitDate < datetime(2008, 1, 1):
+            return set()
+        stars = repo.get_stargazers().totalCount
+        if stars < 10:
+            return set()
+        commiters = repo.get_contributors().totalCount
+        if commiters < 3:
+            return set()
+        return self.inspectFiles(repo, searches)
+
+    def inspectFiles(self, repo, searches, path=""):
+        folder = repo.get_contents(path)
+        res = set()
+        for f in folder:
+            if f.type == "dir":
+                res |= self.inspectFiles(repo, searches, f.path)
+            else:
+                for code, predicates in searches.items():
+                    if any(map(lambda p: p(str(base64.b64decode(f.content)), f.name), predicates)):
+                        res.add(code)
+        return res
+
 
     def search_code(self, search):
         self.__search__(search, lambda g: g.search_code)
