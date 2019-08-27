5ea503fc13bbb53fca55a9b982bb8761cc1f1e30
MicheleGuerriero <michele.guerriero2391@gmail.com>
2019-08-27 19:13:25 +0200

    Fix.

--- ml/setup_input.py
+++ ml/setup_input.py
@@ -8,18 +8,18 @@ OUTPUT_DIR="ansible-results"
 
 # extract dictionary of distinct commits and corresponding repos from input file
 
-commits=[]
+commits={}
 
 commits_perfile=pd.read_csv(INPUT_FILEPATH, index_col=0)
 
 for index, row in commits_perfile.iterrows():
     if not row[1] in commits:
         if row['deletion'] is not np.nan:
-            commits.append([re.sub('^%s' % "git","https", row['repo_id'].replace(".git","")) + "/commit/" + row['commit'], row['deletion']])
+            commits.update({row[2] + "@" + re.sub('^%s' % "git","https", row['repo_id'].replace(".git","")) + "/commit/" + row['commit']: row['deletion']})
 
 
 with open(os.path.join(OUTPUT_DIR,'commits.txt'), 'a') as out1:
     with open(os.path.join(OUTPUT_DIR,'deletions.txt'), 'a') as out2:
         for c in commits:
-            out1.write(c[0] + "\n")
-            out2.write(c[1] + '\nBREAKS HERE\n')
+            out1.write(c + "\n")
+            out2.write(commits[c] + '\nBREAKS HERE\n')
